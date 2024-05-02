2d0dbe0859fe4ac4c0d77c69213476d9a5aedb91
francis-codex <francislogo2219@gmail.com>
2024-05-02 10:39:50 +0100

    added maps class

--- maps.dart
+++ maps.dart
@@ -1,3 +1,31 @@
 void main() {
+//maps: key/Value pairs
+  var cars = {"john": "benz", "mary": "lexus", "dave": "holx"};
+  print(cars);
+  print(cars["dave"]);
 
+  //show values
+  print(cars.values);
+
+  //show keys
+  print(cars.keys);
+
+  //show length
+  print(cars.length);
+
+  //add something
+  cars["peter"] = "camry";
+  print(cars);
+
+  //add many things
+  cars.addAll({"cane": "hyunda", "codex": "truck"});
+  print(cars);
+
+  // remove everything
+  cars.remove("codex");
+  print(cars);
+
+  //remove everything
+  cars.clear();
+  print(cars);
 }
