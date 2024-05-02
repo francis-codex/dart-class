c0faceefa08ea1b782fbb7bdbb2a64682e9cb3f5
francis-codex <francislogo2219@gmail.com>
2024-05-02 11:47:42 +0100

    added loops class

---
+++ loops.dart
@@ -0,0 +1,19 @@
+void main() {
+  // for loop
+  var num = 5;
+  for (var i = num; i >= 1; i--) {
+    print(i);
+  }
+
+  // for in loop
+  var names = ["john", "tina", "steve"];
+  for (var name in names) {
+    print(name);
+  }
+
+  // while loop
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
+}
